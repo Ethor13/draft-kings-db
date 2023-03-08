9bcb6f9bfbbcf29014a33f4c8d0a278c6774485f
ethor13 <ethaniorlowsky@gmail.com>
2023-03-08 15:20:43 -0500

    remove tmp folder from repo

--- workflow.sh
+++ workflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python scripts/python/get_urls.py && \
-python scripts/python/selenium_scraper.py && \
+python scripts/python/selenium_scraper.py # && \
 # ./scripts/shell/download_standings.sh && \
 # ./scripts/shell/format_standings.sh && \
 # python scripts/python/parse_csvs.py && \
