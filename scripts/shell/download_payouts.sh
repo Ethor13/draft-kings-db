6392d052e7ddcba04d05742bc5f355bf5f13d1e5
ethor13 <ethaniorlowsky@gmail.com>
2023-03-23 11:55:50 -0400

    change verbosity of wget

--- scripts/shell/download_payouts.sh
+++ scripts/shell/download_payouts.sh
@@ -4,6 +4,6 @@ rm -rf downloads
 mkdir downloads
 cd downloads
 cat ../flags.csv | wc -l | awk '{print "Downloading " $1 " Files"}'
-cat ../flags.csv | xargs -L 1 -P 10 wget --content-disposition -U
+cat ../flags.csv | xargs -L 1 -P 10 wget --no-verbose --content-disposition -U
 ls | wc -l | awk '{print "Downloaded " $1 " Files"}'
 cd ../..
