d9521c84bcf1835aaa70bb7f00a7d7c77a82800b
ethor13 <ethaniorlowsky@gmail.com>
2023-04-07 19:26:59 -0400

    slight changes

--- workflow.sh
+++ workflow.sh
@@ -2,16 +2,25 @@
 mkdir tmp
 
 # download contests
-python scripts/python/scrape_contests.py
+if [ ! "python scripts/python/scrape_contests.py" ]
+then
+    echo "No contests to scrape today"
+fi
 
 # download payouts
-if python scripts/python/get_payouts_urls.py; then 
+if [ ! "python scripts/python/get_payouts_urls.py" ]
+then 
+    echo "No contests to scrape today"
+else
     ./scripts/shell/download_payouts.sh && \
-     python scripts/python/parse_payouts_json.py
+    python scripts/python/parse_payouts_json.py
 fi
 
 # download standings
-if python scripts/python/get_standings_urls.py; then
+if [ ! "python scripts/python/get_standings_urls.py" ]
+then
+    echo "No contests from two days ago"
+else
     python scripts/python/selenium_scraper.py && \
     ./scripts/shell/download_standings.sh && \
     ./scripts/shell/format_standings.sh && \
